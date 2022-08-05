7e7fa1c35fef3c8e4aad0072b9c61480018c6b8b
SmartPolarBear <clevercoolbear@outlook.com>
2022-08-05 23:39:55 +0800

    feat(xception): implement xception

--- xception/model/xception.py
+++ xception/model/xception.py
@@ -125,3 +125,96 @@ class ExitBlock(nn.Module):
 
         x = residual + x
         return x
+
+
+class EntryFlow(nn.Module):
+    block_in_channels = (64, 128, 256)
+    block_out_channels = (128, 256, 728)
+
+    def __init__(self, in_channels: int):
+        super(EntryFlow, self).__init__()
+
+        self.conv1 = ConvBNReLULayer(
+            in_channels=in_channels, out_channels=32,
+            kernel_size=3, padding=1,
+            stride=2, separable=False,
+            act=False, act_before=False
+        )
+
+        self.conv2 = ConvBNReLULayer(
+            in_channels=32, out_channels=64,
+            kernel_size=3, padding=1,
+            stride=1, separable=False,
+            act=False, act_before=False
+        )
+
+        self.blocks = nn.Sequential(
+            *[EntryBlock(in_channels=self.block_in_channels[i],
+                         out_channels=self.block_out_channels[i])
+              for i in range(3)]
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv1(x)
+        x = self.conv2(x)
+
+        x = self.blocks(x)
+
+        return x
+
+
+class MiddleFlow(nn.Module):
+    def __init__(self, repeat: int = 8):
+        super(MiddleFlow, self).__init__()
+
+        self.blocks = nn.Sequential(
+            *[MiddleBlock(in_channels=728)
+              for i in range(repeat)]
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.blocks(x)
+
+        return x
+
+
+class ExitFlow(nn.Module):
+    def __init__(self, out_channels: int = 2048):
+        self.block = ExitBlock(in_channels=728, out_channels=1024)
+
+        self.conv1 = ConvBNReLULayer(in_channels=1024, out_channels=1536,
+                                     kernel_size=3, stride=1, padding=1, separable=True,
+                                     act=True, act_before=False)
+
+        self.conv2 = ConvBNReLULayer(in_channels=1536, out_channels=out_channels,
+                                     kernel_size=3, stride=1, padding=1, separable=True,
+                                     act=True, act_before=False)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.block(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        return x
+
+
+class Xception(nn.Module):
+    def __init__(self, in_channels: int = 3, classes: int = 10, middle_rep=8):
+        super(Xception, self).__init__()
+
+        self.entry = EntryFlow(in_channels=in_channels)
+        self.middle = MiddleFlow(repeat=middle_rep)
+        self.exit = ExitFlow(out_channels=2048)
+
+        self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Flatten(start_dim=1),
+            nn.Linear(in_features=2048, out_features=classes)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.entry(x)
+        x = self.middle(x)
+        x = self.exit(x)
+
+        x = self.classifier(x)
+        return x
